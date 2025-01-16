015ec2330040d644fffa6b04637f6dfc3367edf8
Chaos Pjeles <chaos@efqr.dev>
2025-01-16 11:35:07 +0100

    modularize package for siemrules (#15)
    * modularize package for siem rules
    
    * Create create_release.yml
    
    ---------
    
    Co-authored-by: David G <himynamesdave@users.noreply.github.com>

--- txt2detection/__main__.py
+++ txt2detection/__main__.py
@@ -4,12 +4,16 @@ from datetime import datetime
 from dataclasses import dataclass
 from functools import partial
 from itertools import chain
+import json
 import os
 from pathlib import Path
 import logging
 import sys
+import uuid
+from stix2 import Identity, parse as parse_stix
 
 from txt2detection.ai_extractor.base import BaseAIExtractor
+from txt2detection.utils import validate_token_count
 
 def configureLogging():
     # Configure logging
@@ -37,7 +41,11 @@ def setLogFile(logger, file: Path):
 from dotenv import load_dotenv
 from .bundler import Bundler
 
+
 from .utils import load_detection_languages, parse_model
+
+def parse_identity(str):
+    return Identity(**json.loads(str))
 
 @dataclass
 class Args:
@@ -46,9 +54,10 @@ class Args:
     tlp_level: str
     labels: list[str]
     created: datetime
-    use_identity: str
+    use_identity: Identity
     detection_language: str
     ai_provider: BaseAIExtractor
+    report_id: uuid.UUID
 
 def parse_created(value):
     """Convert the created timestamp to a datetime object."""
@@ -62,6 +71,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Convert text file to detection format.')
     
     parser.add_argument('--input_file', required=True, help='The file to be converted. Must be .txt')
+    parser.add_argument('--report_id', type=uuid.UUID, help='report_id to use for generated report')
     parser.add_argument('--name', required=True, help='Name of file, max 72 chars. Will be used in the STIX Report Object created.')
     parser.add_argument('--tlp_level', choices=['clear', 'green', 'amber', 'amber_strict', 'red'], default='clear', 
             help='Options are clear, green, amber, amber_strict, red. Default is clear if not passed.')
@@ -69,7 +79,7 @@ def parse_args():
             help='Comma-separated list of labels. Case-insensitive (will be converted to lower-case). Allowed a-z, 0-9.')
     parser.add_argument('--created', type=parse_created, 
             help='Explicitly set created time in format YYYY-MM-DDTHH:MM:SS.sssZ. Default is current time.')
-    parser.add_argument('--use_identity', 
+    parser.add_argument('--use_identity', type=parse_identity,
             help='Pass a full STIX 2.1 identity object (properly escaped). Validated by the STIX2 library. Default is SIEM Rules identity.')
     parser.add_argument('--detection_language', required=True, 
             help='Detection rule language for the output. Check config/detection_languages.yaml for available keys.',
@@ -86,26 +96,25 @@ def parse_args():
     if not os.path.isfile(args.input_file):
         parser.error(f"The specified input file does not exist: {args.input_file}")
 
+    if not args.report_id:
+        args.report_id = Bundler.generate_report_id(args.use_identity.id if args.use_identity else None, args.created, args.name)
+
     return args
 
-def validate_token_count(max_tokens, input, extractor: BaseAIExtractor):
-    logging.info('INPUT_TOKEN_LIMIT = %d', max_tokens)
-    token_count = extractor.count_tokens(input)
-    logging.info('TOKEN COUNT FOR %s: %d', extractor.extractor_name, token_count)
-    if  token_count > max_tokens:
-        raise Exception(f"{extractor.extractor_name}: input_file token count ({token_count}) exceeds INPUT_TOKEN_LIMIT ({max_tokens})")
 
     
 def main(args: Args):
-    setLogFile(logging.root, Path(f"logs/log-{int(args.created.timestamp())}.log"))
+    setLogFile(logging.root, Path(f"logs/log-{args.report_id}.log"))
     input_str = Path(args.input_file).read_text()
     validate_token_count(int(os.getenv('INPUT_TOKEN_LIMIT', 0)), input_str, args.ai_provider)
     detections = args.ai_provider.get_detections(input_str, detection_language=args.detection_language)
-    bundler = Bundler(args.name, args.detection_language.slug, args.use_identity, args.tlp_level, input_str, 0, args.labels)
+    bundler = Bundler(args.name, args.detection_language.slug, args.use_identity, args.tlp_level, input_str, 0, args.labels, report_id=args.report_id)
     bundler.bundle_detections(detections)
     out = bundler.to_json()
 
     output_path = Path("./output")/f"{bundler.bundle.id}.json"
+    data_path = output_path.with_name(f"data--{args.report_id}.json")
     output_path.parent.mkdir(exist_ok=True)
     output_path.write_text(out)
+    data_path.write_text(detections.model_dump_json(indent=4))
     logging.info(f"Writing bundle output to `{output_path}`")
--- txt2detection/ai_extractor/__init__.py
+++ txt2detection/ai_extractor/__init__.py
@@ -5,6 +5,9 @@ import dotenv
 from .base import _ai_extractor_registry as ALL_AI_EXTRACTORS
 
 from .base import BaseAIExtractor
-from .openai import OpenAIExtractor
-from .anthropic import AnthropicAIExtractor
-from .gemini import GeminiAIExtractor
+
+for path in ["openai", "anthropic", "gemini"]:
+    try:
+        __import__(__package__ + "." + path)
+    except Exception as e:
+        logging.warning("%s not installed", path, exc_info=True)
