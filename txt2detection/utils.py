d36ba860638f5b2c5bb566d388cd6738bf7bfff5
Fadl <chaos@efqr.dev>
2025-04-15 11:20:08 +0100

    modify author-identity logic #71

--- txt2detection/utils.py
+++ txt2detection/utils.py
@@ -30,9 +30,22 @@ def parse_model(value: str):
     except Exception as e:
         raise ModelError(f"Unable to initialize model `{value}`") from e
 
-def make_identity(name):
+def make_identity(name, namespace=None, created_by_ref=None, object_marking_refs=None):
     from .bundler import Bundler
-    return Identity(id='identity--'+str(uuid.uuid5(UUID_NAMESPACE, f"txt2detection+{name}")), name=name, created_by_ref=Bundler.default_identity.id, created=datetime(2020, 1, 1), modified=datetime(2020, 1, 1))
+    if isinstance(namespace, str):
+        namespace = uuid.UUID(namespace)
+    namespace = namespace or UUID_NAMESPACE
+    return Identity(
+        id="identity--" + str(uuid.uuid5(namespace, f"{name}")),
+        name=name,
+        created_by_ref=created_by_ref or Bundler.default_identity.id,
+        created=datetime(2020, 1, 1),
+        modified=datetime(2020, 1, 1),
+        object_marking_refs=object_marking_refs or [
+            "marking-definition--94868c89-83c2-464b-929b-a1a8aa3c8487",
+            "marking-definition--a4d70b75-6f4a-5d19-9137-da863edd33d7"
+        ],
+    )
 
 
 def validate_token_count(max_tokens, input, extractor: BaseAIExtractor):
@@ -41,7 +54,7 @@ def validate_token_count(max_tokens, input, extractor: BaseAIExtractor):
     logging.info('TOKEN COUNT FOR %s: %d', extractor.extractor_name, token_count)
     if  token_count > max_tokens:
         raise Exception(f"{extractor.extractor_name}: input_file token count ({token_count}) exceeds INPUT_TOKEN_LIMIT ({max_tokens})")
-    
+
 def valid_licenses():
     @lru_cache(maxsize=5)
     def get_licenses(date):
