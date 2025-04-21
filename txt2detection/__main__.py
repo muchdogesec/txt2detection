b7bb18cf848cc2a5404912c9d56aa496f4e731a7
Fadl <chaos@efqr.dev>
2025-04-21 09:37:28 +0100

    fix created/modified logic #79

--- txt2detection/__main__.py
+++ txt2detection/__main__.py
@@ -106,9 +106,6 @@ def parse_args():
     parser.add_argument("--license", help="Valid SPDX license for the rule", default=None, metavar="[LICENSE]", choices=valid_licenses())
 
     args: Args = parser.parse_args()
-    
-    if args.created is None:
-        args.created = datetime.now()
 
     if not args.sigma_file:
         assert args.ai_provider, "--ai_provider is required in file or txt mode"
@@ -140,7 +137,7 @@ def run_txt2detection(name, identity, tlp_level, input_text: str, labels: list[s
         detection.modified = as_date(kwargs.get('modified'))
         detection.references = kwargs['references']
         detection.detection_id = str(report_id).removeprefix('report--')
-        bundler = Bundler(name, identity, tlp_level or detection.tlp_level or 'clear', detection.description or '', (labels or [])+(detection.tags), report_id=report_id, **kwargs)
+        bundler = Bundler(name, identity, tlp_level or detection.tlp_level or 'clear', detection.description or '', (labels or [])+detection.tags, report_id=report_id, **kwargs)
         detections = DetectionContainer(success=True, detections=[])
         detections.detections.append(detection)
     else:
